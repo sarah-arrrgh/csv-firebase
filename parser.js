d10ae2072a2bdc7f22f4cc6b7530b7dde529d443
[01;31m[K"name"[m[K: "Sarah Rogers <[01;31m[K"email"[m[K: "srogersnz@gmail.com>
2015-11-10 10:25:25 +1300

    Add logic to Firebase push objects and add mocha test suite

--- parser.js
+++ parser.js
@@ -7,18 +7,41 @@ fs.createReadStream("./csv/ceres-june.csv")
   .pipe(csv.parse())
   .pipe(csv.transform(function(record){
     var product = createProductObject(record)
-    console.log(record)
-    myDataRef.push(record)
+    // myDataRef.push(product)
+    console.log(product)
   }))
 
-var createProductObject = function(record){
+var createProductObject = function(record) {
   var object = {}
-  object.code = record[0]
-  object.description = record[3]
-  object.unitSize = record[4]
-  object.quantity = record[5]
-  object.unitTradePrice = record[6]
-  object.caseTradePrice = record[7]
-  return object
+
+  if (record[0] != "" && record[3] == "") {
+    // if record[0] == "Something" && record[3] == "" && it's not all caps, this is a notice - ignore it
+    // if record[0] == "Something" && record[3] == "" && it's all caps, this is a category
+    if (record[0] === record[0].toUpperCase()) {
+      //do nothing
+    } else {
+      object.category = record[0]
+      return object
+    }
+  }
+
+  // if record[0] == "" && record[3] == "Something", this is a brand
+  if (record[0] == "" && record[3] != "") {
+    object.brand = record[3]
+    return object
+  }
+
+  // if record[0] == "Something" && record[3] == "Something", this is a product
+  if (record[0] != "" && record[3] != "") {
+    object.code = record[0]
+    object.description = record[3]
+    object.unitSize = record[4]
+    object.quantity = record[5]
+    object.unitTradePrice = record[6]
+    object.caseTradePrice = record[7]
+    return object
+  }
+
+  // if record[0] == "" && record[3] == "", this is nothing, ignore it
 }
 
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var chai = require("chai")
+var expect = chai.expect
+
+describe("A thing", function(){
+  it("should work", function(){
+    expect(true).to.equal(true)
+  })
+})
